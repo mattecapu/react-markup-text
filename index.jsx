037c76a28be61008f81448b92bfbbedfbdc9a523
Matteo Capucci <mattecapu@live.it>
2016-12-13 19:06:49 +0100

    added param check

--- index.jsx
+++ index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 export default function textToReactMarkup(text) {
+	if (!text || text === '' || (typeof text !== 'string' && !(text instanceof String))) {
+		return [];
+	}
 	return text.split("\n\n")
 		.map((par, i) =>
 			<p key={i}>
--- test.jsx
+++ test.jsx
@@ -5,6 +5,13 @@ import ReactDOM from 'react-dom/server';
 
 import textToReactMarkup from './index.jsx';
 
+const renderTestElement = (text) =>
+	ReactDOM.renderToStaticMarkup(
+		<div>
+			{textToReactMarkup(text)}
+		</div>
+	)
+
 const longText = `Nel mezzo del cammin di nostra vita
 mi ritrovai per una selva oscura,
 ché la diritta via era smarrita.
@@ -20,10 +27,26 @@ dirò de l’altre cose ch’i’ v’ho scorte.`;
 const reactMarkup = `<div><p><span>Nel mezzo del cammin di nostra vita<br/></span><span>mi ritrovai per una selva oscura,<br/></span><span>ché la diritta via era smarrita.<br/></span></p><p><span>Ahi quanto a dir qual era è cosa dura<br/></span><span>esta selva selvaggia e aspra e forte<br/></span><span>che nel pensier rinova la paura!<br/></span></p><p><span>Tant’è amara che poco è più morte;<br/></span><span>ma per trattar del ben ch’i’ vi trovai,<br/></span><span>dirò de l’altre cose ch’i’ v’ho scorte.<br/></span></p></div>`;
 
 assert.equal(
-	ReactDOM.renderToStaticMarkup(
-		<div>
-			{textToReactMarkup(longText)}
-		</div>
-	),
+	renderTestElement(longText),
 	reactMarkup
+);
+
+assert.equal(
+	renderTestElement(''),
+	'<div></div>'
+);
+
+assert.equal(
+	renderTestElement(),
+	'<div></div>'
+);
+
+assert.equal(
+	renderTestElement(null),
+	'<div></div>'
+);
+
+assert.equal(
+	renderTestElement({ rubbish: true }),
+	'<div></div>'
 );
